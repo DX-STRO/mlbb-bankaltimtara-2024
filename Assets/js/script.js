c2425fa03345062ab4fe0490b515ff8f5d07a81a
DX-STRO <aldinnuradzin@gmail.com>
2024-06-05 23:30:55 +0800

    view: home

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,20 @@
-// window.addEventListener('scroll', () => {
-//     const wScroll = window.scrollY;
-//     console.log('wScroll');
-//     $(".header .hero").css({
-//         'transform': 'translate(0%,' + wScroll / 20 + '%)',
-//         'transition': '.7s ease-out',
-//     });
+window.addEventListener('scroll', () => {
+    const wScroll = window.scrollY;
+    // console.log('wScroll');
+    $("header .top-left-rect1").css({
+        'transform': 'translate(0%,' + wScroll / 20 + '%)',
+        'transition': '.7s ease-out',
+    });
+    
+   var homeTop = $("header").offset().top - 0;
+   var aboutTop = $(".about-section").offset().top - 100;
 
-// });
+   if (wScroll >= aboutTop) {
+       $("nav ul li a").removeClass("active");
+       $("nav ul li a.about").addClass("active");
+   } else if (wScroll < aboutTop && wScroll >= homeTop) {
+       $("nav ul li a").removeClass("active");
+       $("nav ul li a.home").addClass("active");
+   }
+});
 
-
-if (wScroll > $(".container").offset().top - 10) {
-    $(".container nav").addClass("show");
-    $(".landing-page .nav").addClass("remove");
-}
